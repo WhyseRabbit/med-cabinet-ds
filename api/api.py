c323b4a4a4c54cb1871c9e7ce8e8e77c292aea53
WhyseRabbit <aae@centaur.me>
2021-01-05 13:15:11 -0700

    implementing csv data

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import pandas as pd
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,6 +8,11 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET"])
 def home():
     return "This is the beginning of the med-cabinet API."
+
+
+@app.route("/strains/refresh/add_all", methods=["GET"])
+def add_all():
+    strain_list = pd.read_csv("../cannabis.csv")
 
 
 app.run()
